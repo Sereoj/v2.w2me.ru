f8beb3820002959b5177c0b1dc5901730ae381ed
Sergey Egorov <ser.12egorov@gmail.com>
2021-09-26 17:14:55 +0300

    add models

---
+++ app/Models/catalog_download.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class catalog_download extends Model
+{
+    use HasFactory;
+}

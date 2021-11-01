fd28c5fc7816ec392da1f4566e8483b837bf0931
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-01 13:47:11 +0300

    fix

---
+++ app/Http/Resources/CatalogResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CatalogResource extends JsonResource
+{
+
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'meta_title' => $this->meta_title,
+            'meta_description' => $this->meta_description,
+            'preview' => $this->preview,
+            'images' => $this->images,
+            'category' => $this->category,
+            'license' => $this->license,
+            'user' => $this->user->name,
+            'download' => $this->download,
+            'rating' => $this->rating,
+        ];
+    }
+}

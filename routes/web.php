3c396f43d37936973b283b99cfbba0c7b15429cc
Sergey Egorov <ser.12egorov@gmail.com>
2021-11-03 13:56:00 +0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UI\ThumbnailsController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,39 +16,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index'])->name('index');
+Route::get('/',[ThumbnailsController::class, 'index'])->name('index');
 
 Route::name('images.')->group(
     function ()
     {
-        Route::get('/new',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_new'])->name('new');
-        Route::get('/popular',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_popular'])->name('popular');
-        Route::get('/wait',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_wait'])->name('wait');
+        Route::get('/new',[ThumbnailsController::class, 'index_new'])->name('new');
+        Route::get('/popular',[ThumbnailsController::class, 'index_popular'])->name('popular');
+        Route::get('/wait',[ThumbnailsController::class, 'index_wait'])->name('wait');
 
-        Route::middleware('auth')->get('/favorite',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_favorite'])->name('favorite');
-        Route::middleware('auth')->get('/install',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_install'])->name('install');
-        Route::middleware('auth')->get('/load',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_load'])->name('load');
-        Route::middleware('auth')->post('/load',[\App\Http\Controllers\UI\ThumbnailsController::class, 'store_load']);
+        Route::middleware('auth')->get('/favorite',[ThumbnailsController::class, 'index_favorite'])->name('favorite');
+        Route::middleware('auth')->get('/install',[ThumbnailsController::class, 'index_install'])->name('install');
+        Route::middleware('auth')->get('/load',[ThumbnailsController::class, 'index_load'])->name('load');
+        Route::middleware('auth')->post('/load',[ThumbnailsController::class, 'store_load']);
 
-        Route::get('/images/{id}',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_simple'])->name('simple');
+        Route::get('/images/{id}',[ThumbnailsController::class, 'index_simple'])->name('simple');
     }
 );
 
+Route::get('/email/verify', function () {
+    return view('layouts.verify-email');
+})->middleware('auth')->name('verification.notice');
 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return route('index');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::name('user.')->group(
     function ()
     {
-        Route::get('/register', [\App\Http\Controllers\Auth\AuthController::class,'index_register'])->name('register');
-        Route::post('/register', [\App\Http\Controllers\Auth\AuthController::class, 'create']);
+        Route::get('/register', [AuthController::class,'index_register'])->name('register');
+        Route::post('/register', [AuthController::class, 'create']);
 
-        Route::get('/login', [\App\Http\Controllers\Auth\AuthController::class, 'index_login'])->name('login');
-        Route::post('/login', [\App\Http\Controllers\Auth\AuthController::class, 'store']);
+        Route::get('/login', [AuthController::class, 'index_login'])->name('login');
+        Route::post('/login', [AuthController::class, 'store']);
 
-        Route::middleware('auth')->get('/profile', [\App\Http\Controllers\Auth\AuthController::class, 'index_profile'])->name('profile');
-        Route::middleware('auth')->get('/dashboard', [\App\Http\Controllers\Auth\AuthController::class, 'index_profile'])->name('dashboard');
+        Route::middleware('auth')->get('/profile', [AuthController::class, 'index_profile'])->name('profile');
+        Route::middleware('auth')->post('/profile', [AuthController::class, 'store_profile']);
 
-        Route::middleware('auth')->get('/logout',[\App\Http\Controllers\UI\ThumbnailsController::class, 'index_logout'])->name('logout');
+        Route::middleware('auth')->get('/profile/edit', [AuthController::class, 'index_edit_profile'])->name('edit');
+        Route::middleware('auth')->post('/profile/edit', [AuthController::class, 'store_edit_profile']);
+
+        Route::middleware('auth')->get('/dashboard', [AuthController::class, 'index_profile'])->name('dashboard');
+
+        Route::middleware('auth')->get('/logout',[ThumbnailsController::class, 'index_logout'])->name('logout');
 
     }
 );
